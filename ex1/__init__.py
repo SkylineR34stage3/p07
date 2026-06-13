78d1e8b4e1ed2a10ab743a9068ca4624147626c3
Artem Sokolov <artemschannel2@gmail.com>
2026-06-13 16:14:08 +0200

    ex1: Added required classes; testing tbd

---
+++ ex1/__init__.py
@@ -0,0 +1,4 @@
+__all__ = [
+    "CreatureFactory", "HealingCreatureFactory", "TransformCreatureFactory"]
+from ..ex0 import CreatureFactory
+from .factories import HealingCreatureFactory, TransformCreatureFactory
