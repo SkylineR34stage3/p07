769fde6130cb78677c3c3371b12ed7efd95911c0
Artem Sokolov <artemschannel2@gmail.com>
2026-06-14 15:00:52 +0200

    ex2: Added main logic for tounament.py; Output tuning + flake8 & mypy TBD

--- ex2/__init__.py
+++ ex2/__init__.py
@@ -1,6 +1,7 @@
 __all__ = [
-    "BattleStrategy", "NormalStrategy",
-    "AggressiveStrategy", "DefensiveStrategy"
+    "BattleStrategy", "NormalStrategy", "AggressiveStrategy",
+    "DefensiveStrategy", "InvalidStrategyError"
     ]
 from .strategies import BattleStrategy, NormalStrategy
 from .strategies import AggressiveStrategy, DefensiveStrategy
+from .exceptions import InvalidStrategyError
--- tournament.py
+++ tournament.py
@@ -1,24 +1,71 @@
-from ex0 import CreatureFactory
-from ex2 import BattleStrategy
+from ex0 import CreatureFactory, FlameFactory, AquaFactory
+from ex1 import HealingCreatureFactory, TransformCreatureFactory
+from ex2 import BattleStrategy, NormalStrategy, DefensiveStrategy
+from ex2 import AggressiveStrategy, InvalidStrategyError
 
 
 def fight(
         opponent1: tuple[CreatureFactory, BattleStrategy],
         opponent2: tuple[CreatureFactory, BattleStrategy]
         ) -> None:
-    pass
+    factory1, strategy1 = opponent1
+    factory2, strategy2 = opponent2
+    creature1 = factory1.create_base()
+    creature2 = factory2.create_base()
+
+    print("\n* Battle *")
+    print(creature1.describe(), " vs.",
+          creature2.describe(), sep="\n")
+    print(" now fight!")
+    strategy1.act(creature1)
+    strategy2.act(creature2)
 
 
 def battle(
         opponents: list[tuple[CreatureFactory, BattleStrategy]]
         ) -> None:
-    for i in range(len(opponents)):
-        for j in range(i + 1, len(opponents)):
+    print("*** Tournament ***")
+    opponents_num = len(opponents)
+    print(f"{opponents_num} opponents involved")
+    for i in range(opponents_num):
+        for j in range(i + 1, opponents_num):
             fight(opponents[i], opponents[j])
 
 
+def err_catcher(
+        opponents: list[tuple[CreatureFactory, BattleStrategy]]
+        ) -> None:
+    try:
+        battle(opponents)
+    except InvalidStrategyError as e:
+        print(f"Battle error, aborting tournament: {e}")
+
+
 def main() -> None:
-    pass
+    print("Tournament 0 (basic)")
+    opponents = [
+        (FlameFactory(), NormalStrategy()),
+        (HealingCreatureFactory(), DefensiveStrategy())
+        ]
+    print(f" {opponents}")
+    err_catcher(opponents)
+
+    print("\nTournament 1 (error)")
+    opponents = [
+        (FlameFactory(), AggressiveStrategy()),
+        (HealingCreatureFactory(), DefensiveStrategy())
+        ]
+    print(f" {opponents}")
+    err_catcher(opponents)
+
+    print("\nTournament 2 (multiple)")
+    opponents = [
+        (AquaFactory(), NormalStrategy()),
+        (HealingCreatureFactory(), DefensiveStrategy()),
+        (TransformCreatureFactory(), AggressiveStrategy())
+        ]
+    print(f" {opponents}")
+    err_catcher(opponents)
 
 
 if __name__ == "__main__":
